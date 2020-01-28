53cb7a937b8b2e71247d7cdf5496ffa39122a432
eroztekin <eroztekin@itu.edu.tr>
2020-01-28 05:09:59 +0300

    add value examples

---
+++ src/values.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// String concatenation
+	fmt.Println("Golang " +  "2nd Tutorial")
+
+	fmt.Println("5 + 7 =", 5 + 7)
+	fmt.Println("12 / 3 =", 12 / 3)
+	fmt.Println("7.5 / 2.5 =", 7.5 / 2.5)
+
+	fmt.Println("true && false =", true && false)
+	fmt.Println("true || false =", true || false)
+	fmt.Println("!false =", !false)
+}
