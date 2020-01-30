6b40b26156cd80de688d22082305bc0a4c5d37e2
eroztekin <eroztekin@itu.edu.tr>
2020-01-30 03:25:26 +0300

    add more slice examples

--- src/slices.go
+++ src/slices.go
@@ -14,9 +14,32 @@ func main() {
 	fmt.Println("slice:", mySlice)
 	fmt.Println("slice length:", len(mySlice))
 
-	// append values (one of the features of slices)
+	// Some of the features of Slices
+	// Append
 	mySlice = append(mySlice, "Lemon")
 	fmt.Println("slice:", mySlice)
 	fmt.Println("slice length:", len(mySlice))
 
+	// Copy
+	copySlice := make([]string, 4)
+	copy(copySlice, mySlice) // copy to copySlice from mySlice
+	fmt.Println("Copy slice:", copySlice)
+
+	// Slice
+	fmt.Println("slice[1:3]:", mySlice[1:3])
+	fmt.Println("slice[:3]:", mySlice[:3])
+	fmt.Println("slice[2:]:", mySlice[2:])
+
+	// Declare & Initialize in a single line
+	mySlice2 := []string{"Book", "Mac", "Programming"}
+	fmt.Println("mySlice2:", mySlice2)
+
+	twoDSlice := make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		twoDSlice[i] = make([]int, 5)
+		for j := 0; j < 5; j++ {
+			twoDSlice[i][j] = i + j
+		}
+	}
+	fmt.Println("twoDSlice:", twoDSlice)
 }
